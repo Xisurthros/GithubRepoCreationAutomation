fd9e41e1c408976f2062cb4bb61f620137523dcc
xisurthros <jacobcoburnhobbs@gmail.com>
2023-11-20 23:07:41 -0600

    added TODO

---
+++ TODO.py
@@ -0,0 +1,10 @@
+"""
+TODO:
+    Update to os.environ variables
+    Remove secrets.py
+    Remove hardcoded username
+    Add generic batch file
+    Create README.md
+        Explain how to use
+        How to get it setup
+"""
