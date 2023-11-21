69fd6bc09f6e9e31c3ab28f8aaa2ae29e7694a85
xisurthros <jacobcoburnhobbs@gmail.com>
2023-11-20 22:55:02 -0600

    overhauled original development

---
+++ local/classes/helpers.py
@@ -0,0 +1,114 @@
+import json
+import requests
+from secrets import TOKEN
+
+
+class Helpers:
+
+    def __init__(self, username: str):
+        self.username = username
+        self.headers = {
+            'Authorization': f'token {TOKEN}',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        self._base_url = 'https://api.github.com/'
+        self._url = None
+
+    def __getattribute__(self, attr: str) -> object:
+        """
+        Usage: get all callable functions from the class and set the url based on the function name
+
+        :param attr:
+        :return:
+        """
+        attribute = object.__getattribute__(self, attr)
+
+        class_name = object.__getattribute__(self, '__class__')
+        allowedAttributes = [func for func in dir(class_name) if
+                             callable(getattr(class_name, func)) and not any(x in func for x in ['__', 'help'])]
+
+        if callable(attribute) and attr in allowedAttributes:
+            def wrapper(*args, **kwargs):
+                self.url = attr
+                return attribute(*args, **kwargs)
+
+            return wrapper
+        return attribute
+
+    @property
+    def url(self) -> str:
+        """
+        Usage: get url of the current endpoint
+
+        :return:
+        """
+        return self._url
+
+    @url.setter
+    def url(self, var: str) -> None:
+        """
+        Usage: set url to a specific endpoint based on the function name
+
+        :param var:
+        :return:
+        """
+        urls = {
+            'help': 'help',
+            'get_repos': 'search/repositories?q=user:',
+            'make_repo': 'user/repos'
+        }
+        self._url = f'{self._base_url}{urls[var]}'
+
+    @staticmethod
+    def help(*args) -> str:
+        """
+        Usage: print out all available functions and their usage
+
+        :param args:
+        :return:
+        """
+        _ = args
+        return """
+        Usage: python main.py <function_name> <repo_name> <repo_visibility>
+        Available functions:
+            help: shows this message
+            make_repo: creates a new repo
+            get_repos: gets all repos from a user
+        """
+
+    def get_repos(self) -> list[dict[str, str]]:
+        """
+        Usage: python main.py get_repos
+
+        :return:
+        """
+        data = requests.get(f'{self._url}{self.username}', headers=self.headers).json()
+        return [{f"{data['items'][i]['name']}": f"{data['items'][i]['ssh_url']}"} for i in range(len(data['items']))]
+
+    def make_repo(self, *args) -> dict:
+        """
+        Usage: python main.py make_repo <repo_name> <repo_visibility>
+
+        :param args:
+        :return:
+        """
+        repo_name = args[0]
+        visibility = args[1]
+        repo_visibility = {
+            'public': False,
+            'private': True
+        }
+        repo_data = {
+            "name": repo_name,
+            "private": repo_visibility[visibility]
+        }
+        json_data = json.dumps(repo_data)
+
+        response = requests.post(self.url, headers=self.headers, data=json_data)
+        response = response.json()
+        info = {
+            'name': response['name'],
+            'visibility': response['visibility'],
+            'clone_url': response['clone_url']
+        }
+        return info
---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+from local.classes.helpers import Helpers
+
+if __name__ == '__main__':
+    helpers = Helpers(username='Xisurthros')
+
+    runnable_functions = {
+        "help": helpers.help,
+        "make_repo": helpers.make_repo,
+        "get_repos": helpers.get_repos
+    }
+
+    try:
+        function_name = sys.argv[1] if len(sys.argv) > 1 else None
+        if function_name in runnable_functions:
+            args = sys.argv[2:]
+            print(runnable_functions[function_name](*args))
+        else:
+            print("Invalid function name" if function_name else "No arguments provided. Please specify a function name.")
+            helpers.help()
+    except IndexError:
+        print("Error: Insufficient arguments provided.")
